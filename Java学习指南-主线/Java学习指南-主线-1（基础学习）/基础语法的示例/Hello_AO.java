73a38a3b72c375c72c0168417109813677db7ce5
TanShould <50075439+TanShould@users.noreply.github.com>
2021-05-09 22:03:35 +0800

    Add files via upload

---
+++ Java学习指南-主线/Java学习指南-主线-1（基础学习）/基础语法的示例/Hello_AO.java
@@ -0,0 +1,23 @@
+package my;
+
+public class Hello_AO
+{
+
+	public static void main(String[] args)
+	{
+		int a = 10;
+		int b = 5;
+		int c = a + b;
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = a + b =" + c);
+		
+		int d = 2;
+		int e = 4;
+		int f = d * e;
+		System.out.println("d = " + d);
+		System.out.println("e = " + e);
+		System.out.println("f = d * e = " + f);
+	}
+
+}
