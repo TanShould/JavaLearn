825c7408de501ce83368b080f38119b3cb397209
TanShould <50075439+TanShould@users.noreply.github.com>
2021-05-10 20:39:13 +0800

    Add files via upload

---
+++ Java学习指南-主线/Java学习指南-主线-1（基础学习）/基础语法的示例/Hello_if.java
@@ -0,0 +1,24 @@
+package my;
+
+public class Hello_if
+{
+
+	public static void main(String[] args)
+	{
+		int a = 10;
+		int b = 9;
+		
+		if ( a==10 && b==10 )
+		{
+			System.out.println("a和b的值均为10");
+		}
+		
+		if ( a== 10 || b ==10 )
+		{
+			System.out.println("a和b其中一个数的值为10");
+		}
+		
+		
+	}
+
+}
