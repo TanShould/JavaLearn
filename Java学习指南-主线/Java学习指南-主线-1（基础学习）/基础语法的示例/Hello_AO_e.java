b60ef322a21ca3ee80c038d7522a5c71217eb1bb
TanShould <50075439+TanShould@users.noreply.github.com>
2021-05-10 20:31:13 +0800

    Add files via upload

---
+++ Java学习指南-主线/Java学习指南-主线-1（基础学习）/基础语法的示例/Hello_AO_e.java
@@ -0,0 +1,18 @@
+package my;
+
+public class Hello_AO_e
+{
+
+	public static void main(String[] args)
+	{
+		//自增与自减
+		int a = 9;
+		System.out.println("a = " + a);
+		a++ ;
+		System.out.println("a = " + a);
+		a--;
+		System.out.println("a = " + a);
+		
+	}
+
+}
